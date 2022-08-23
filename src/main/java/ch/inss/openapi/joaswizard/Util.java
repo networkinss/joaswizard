3e1f7f0e169a4aff205eaae9e555a37333bd415a
networkinss <oliver.glas@inss.ch>
2022-08-23 21:14:03 +0200

    remove commons-io

--- src/main/java/ch/inss/openapi/joaswizard/Util.java
+++ src/main/java/ch/inss/openapi/joaswizard/Util.java
@@ -1,7 +1,6 @@
 package ch.inss.openapi.joaswizard;
 
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
-import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -9,14 +8,16 @@ import org.json.simple.parser.ParseException;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.function.Consumer;
 
 public class Util implements Constants {
-    
+
 //    private static Logger logger = null;
 //    
 //    static {
@@ -85,14 +86,23 @@ public class Util implements Constants {
 
     String readFromClasspath(String file) {
 //        File filePath = new File(file);
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream(file);
-        StringWriter writer = new StringWriter();
-        try {
-            IOUtils.copy(in, writer, "UTF-8");
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(file);
+//        StringWriter writer = new StringWriter();
+        StringBuilder textBuilder = new StringBuilder();
+//        try {
+//            IOUtils.copy(in, writer, "UTF-8");
+        try (
+                Reader reader = new BufferedReader(new InputStreamReader
+                        (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                textBuilder.append((char) c);
+            }
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return writer.toString();
+        return textBuilder.toString();
     }
 
     /*** #1
@@ -106,23 +116,25 @@ public class Util implements Constants {
         return result;
     }
 
-    static HashMap<String, Object> readYamlFile(String inputFile) {
-        if (inputFile == null) {
-            inputFile = "src/test/resources/Pet.yml";
-        }
-        File file = new File(inputFile);
-        Yaml yaml = new Yaml();
-        InputStream fis = null;
-        try {
-            fis = new FileInputStream(inputFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-        return yaml.load(isr);
-    }
+//    static HashMap<String, Object> readYamlFile(String inputFile) {
+//        if (inputFile == null) {
+//            inputFile = "src/test/resources/Pet.yml";
+//        }
+//        File file = new File(inputFile);
+//        Yaml yaml = new Yaml();
+//        InputStream fis = null;
+//        try {
+//            fis = new FileInputStream(inputFile);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+//        return yaml.load(isr);
+//    }
 
-    /** regex =  "[0-9.]+", null = false, */
+    /**
+     * regex =  "[0-9.]+", null = false,
+     */
     static boolean isNumber(String str) {
         return str != null && str.trim().matches("[0-9.]+");
     }
@@ -142,20 +154,20 @@ public class Util implements Constants {
             JSONParser parser = new JSONParser();
             JSONArray obj = (JSONArray) parser.parse(file);
             Consumer<Object> lambdaExpression = x -> {
-                JSONObject jsonObject = (JSONObject) x;    
-                HashMap<String,String> map = new HashMap<>();
+                JSONObject jsonObject = (JSONObject) x;
+                HashMap<String, String> map = new HashMap<>();
                 String dbtype = (String) jsonObject.get(Header.DBTYPE.toString().toLowerCase());
                 String oastype = (String) jsonObject.get(Header.OASTYPE.toString().toLowerCase());
                 String oasformat = (String) jsonObject.get(Header.OASFORMAT.toString().toLowerCase());
                 String oaspattern = (String) jsonObject.get(Header.OASPATTERN.toString().toLowerCase());
-                map.put(Header.OASTYPE.toString(),oastype);
+                map.put(Header.OASTYPE.toString(), oastype);
                 map.put(Header.OASFORMAT.toString(), oasformat);
                 map.put(Header.OASPATTERN.toString(), oaspattern);
                 resultMap.put(dbtype, map);
             };
-            obj.stream().forEach( lambdaExpression);
-            
-        }catch(ParseException pe){
+            obj.stream().forEach(lambdaExpression);
+
+        } catch (ParseException pe) {
             pe.printStackTrace();
             return null;
         }
@@ -165,17 +177,17 @@ public class Util implements Constants {
     /**
      * Splits a String like a filename into the part before the file extension (like .yml) and the extension itself without dot.
      */
-    static String[] splitExtension(String name) {
-        int pos = 0;
-        String[] result = new String[2];
-        pos = name.lastIndexOf(".");
-        if (pos <= 1) {
-            result[0] = name;
-            return result;
-        }
-        result[0] = name.substring(0, pos);
-        result[1] = name.substring(pos + 1, name.length());
-        return result;
-    }
+//    static String[] splitExtension(String name) {
+//        int pos = 0;
+//        String[] result = new String[2];
+//        pos = name.lastIndexOf(".");
+//        if (pos <= 1) {
+//            result[0] = name;
+//            return result;
+//        }
+//        result[0] = name.substring(0, pos);
+//        result[1] = name.substring(pos + 1, name.length());
+//        return result;
+//    }
 
 }
